0671e2a4ce342adcb48e4c0d2c9c017185d5c3db
MOUAD FIFEL <mouad.fifel5@gmail.com>
2022-12-01 00:14:49 +0100

    navbar | working ...

---
+++ include/navbar.php
@@ -0,0 +1,34 @@
+
+<?php include"header.php" ?>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container">
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#"><h2 style="color: rgb(139, 24, 56)"> <b> Youteckets </b> </h2></a>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarButtonsExample"  aria-hidden="true"></i>
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+
+      <div class="d-flex align-items-center">
+        <button type="button" class="btn btn-link px-3 me-2">
+          Login
+        </button>
+        <button type="button" class="btn btn-primary me-3">
+          Sign 
+        </button>
+      </div>
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
