d018ef4689de4389032d88c616b53bf3a98ceab1
MOUAD FIFEL <mouad.fifel5@gmail.com>
2022-11-30 23:35:56 +0100

    footer | Done

---
+++ include/footer.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>   
+    
+    <!-- CSS only -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: rgb(139, 24, 56)"
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h3 class="text-uppercase mb-4 font-weight-bold">
+            FIFA World Cup <br>
+              Qatar 2022
+            </h3>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">About</h6>
+            <p>
+              <span class="text-white">About</span>
+            </p>
+            <p>
+              <span class="text-white">How it works</span>
+            </p>
+            <p>
+              <span class="text-white">Careers</span>
+            </p>
+            <p>
+              <span class="text-white">Press</span>
+            </p>
+            <p>
+              <span class="text-white">Blog</span>
+            </p>
+            <p>
+              <span class="text-white">Forum</span>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Partner with us
+            </h6>
+            <p>
+              <span class="text-white">Partnership programs</span>
+            </p>
+            <p>
+              <span class="text-white">Affiliate program</span>
+            </p>
+            <p>
+              <span class="text-white">connectivity Partner</span>
+            </p>
+            <p>
+              <span class="text-white">promotion and events</span>
+            </p>
+            <p>
+              <span class="text-white">integrations</span>
+            </p>
+            <p>
+              <span class="text-white">community</span>
+            </p>
+            <p>
+              <span class="text-white">Loyalty peogram </span>
+            </p>
+          </div>
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Support
+            </h6>
+            <p>
+              <span class="text-white">Help center</span>
+            </p>
+            <p>
+              <span class="text-white"></span>Contact us
+            </p>
+            <p>
+              <span class="text-white">Privacy Policy</span>
+            </p>
+            <p>
+              <span class="text-white">Termes of services</span>
+            </p>
+            <p>
+              <span class="text-white">Trust and safety</span>
+            </p>
+            <p>
+              <span class="text-white">accessibility</span>
+            </p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="my-3">
+
+      <!-- Section: Copyright -->
+      <section class="p-3 pt-0">
+        <div class="row d-flex align-items-center">
+          <!-- Grid column -->
+          <div class="col-md-7 col-lg-8 text-center text-md-start">
+            <!-- Copyright -->
+            <div class="p-3">
+              © 2020 Copyright YouTeckets
+            </div>
+            <!-- Copyright -->
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+            <!-- Facebook -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="bi bi-facebook"></i
+              ></a>
+
+            <!-- Twitter -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="bi bi-twitter"></i
+              ></a>
+
+            <!-- Google -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="bi bi-google"></i
+              ></a>
+
+            <!-- Instagram -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="bi bi-instagram"></i></a>
+          </div>
+          <!-- Grid column -->
+        </div>
+      </section>
+      <!-- Section: Copyright -->
+    </div>
+    <!-- Grid container -->
+  </footer>
+  <!-- Footer -->
+</div>
+<!-- End of .container -->  
+
+
+
+</body>
+</html>        
