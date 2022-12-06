fc0ff83d3b51a2e17a63ba26e98661986503f8de
adnankhayrou <adnanekhayrou3@gmail.com>
2022-12-06 10:21:36 +0100

    redy to push

--- pages/stadpage.php
+++ pages/stadpage.php
@@ -1,3 +1,8 @@
+<?php 
+include '../include/navbar.php';
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,29 +15,192 @@
     <title>Document</title>
 </head>
 <body>
-    <div class=" container  my-3">
-        <div class="row justify-content-center ">
-                <div class=" card col-lg-3 col-md-6 col-sm-12">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            </div>
-            <div class=" card col-lg-3 col-md-6 col-sm-12 ">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            </div>
-            <div class=" card col-lg-3 col-md-6 col-sm-12 ">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+    <div class=" container my-3">
+        <div class="row my-2  ">
+            <!-- cet card pour les équipes  -->
+                <div class="col-lg-3 col-md-6 col-sm-12 my-1 ">
+                    <div class="card ">
+                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
+                        <div class="card-body">
+                            <div>Morocco National Teames</div>
+                            <div>Group F</div>
+                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
+                        <div class="card-body">
+                            <div>Morocco National Teames</div>
+                            <div>Group F</div>
+                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
+                        <div class="card-body">
+                            <div>Morocco National Teames</div>
+                            <div>Group F</div>
+                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
+                        <div class="card-body">
+                            <div>Morocco National Teames</div>
+                            <div>Group F</div>
+                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+           <!-- cet card pour les match  -->
+           <div class=" row my-2 my-ms-1">
+                <div class="col-lg-3 col-md-6 col-sm-12 my-1">
+                    <div class="card ">
+                        <div class="position-relative">
+                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
+                            <img class=" position-absolute top-50 start-0 translate-middle-y" style="width: 6rem;"  src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
+                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class=" d-flex align-items-center">
+                                    NOV<br>25
+                                </div>
+                                <div class="ms-5">
+                                    <div>morocco vs canada</div>
+                                    <div>$ 150</div>
+                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             
-         </div>
-         
-</div>
-</div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <div class="position-relative">
+                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
+                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
+                            <img class="position-absolute top-50 end-0 translate-middle-y"  style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class=" d-flex align-items-center">
+                                    NOV<br>25
+                                </div>
+                                <div class="ms-5">
+                                    <div>morocco vs canada</div>
+                                    <div>$ 150</div>
+                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <div class="position-relative">
+                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
+                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
+                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class=" d-flex align-items-center">
+                                    NOV<br>25
+                                </div>
+                                <div class="ms-5">
+                                    <div>morocco vs canada</div>
+                                    <div>$ 150</div>
+                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card ">
+                        <div class="position-relative">
+                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
+                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
+                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class=" d-flex align-items-center">
+                                    NOV<br>25
+                                </div>
+                                <div class="ms-5">
+                                    <div>morocco vs canada</div>
+                                    <div>$ 150</div>
+                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- cet card pour les stades  -->
+            <div class=" row my-2 ">
+                <div class="col-lg-3 col-md-6 col-sm-12 my-1">
+                    <div class="card">
+                        <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
+                        <div class="card-body">
+                                <div>Khalifa International Stadium</div>
+                                <div>capacity : <span>40,000</span></div>
+                                <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
+                        <div class="card-body">
+                                <div>Khalifa International Stadium</div>
+                                <div>capacity : <span>40,000</span></div>
+                                <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                    <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
+                    <div class="card-body">
+                            <div>Khalifa International Stadium</div>
+                            <div>capacity : <span>40,000</span></div>
+                            <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                    <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
+                    <div class="card-body">
+                            <div>Khalifa International Stadium</div>
+                            <div>capacity : <span>40,000</span></div>
+                            <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
+                    </div>
+                </div>
+                </div>
+            </div>
+           
+    </div>
+    <?php      include '../include/footer.php';?>
 </body>
 </html>
