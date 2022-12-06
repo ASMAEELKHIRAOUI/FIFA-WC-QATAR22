3c1d9a1e3fd77f17d4ec5d2105fc4b9d922f11a7
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-12-06 15:49:53 +0100

    old reservation | modify footer

--- pages/editprofile.php
+++ pages/editprofile.php
@@ -1,9 +1,95 @@
 
+<link href="../assets/css/dashtest.css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="../assets/css/styleF.css">
+<?php include"../include/head.php" ?>
 <?php include"../include/navbar.php" ?>
 
 <div class="alert alert-success m-4 text-center" role="alert">
-  <h3 > Welcome in you profile! </h3> 
+  <h3 > Welcome in you Profile! </h3> 
 </div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="card m-3">
+        <div class="card-body">
+          <div class="overview-wrap">
+            <h2 class="title-1 m-b-25 me-4" id="old-reservation">Old reservations</h2>
+            <hr />
+          </div>
+
+          
+
+                               
+
+                                <br>
+                                <div class="table-responsive table--no-card mb-4" >
+                                    <table class="table table-borderless table-striped table-earning" >
+                                        <thead >
+                                            <tr >
+                                                <th>Image</th>
+                                                <th>Date-Time</th>
+                                                <!-- <th>Time</th> -->
+                                                <th class="text-right">First team</th>
+                                                <th class="text-right">Secound team</th>
+                                                <th class="text-right">Stad</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>test</td>
+                                                <td>2018-09-29 05:57 PM</td>
+                                                <!-- <td>05:57</td> -->
+                                                <td class="text-right">morocco</td>
+                                                <td class="text-start">usa</td>
+                                                <td class="text-right">tamama</td>
+                                            </tr>
+                                            <tr>
+                                                <td>test</td>
+                                                <td>2018-09-29 05:57 PM</td>
+                                                <!-- <td>05:57</td> -->
+                                                <td class="text-right">morocco</td>
+                                                <td class="text-start">usa</td>
+                                                <td class="text-right">tamama</td>
+                                            </tr>
+                                            <tr>
+                                                <td>test</td>
+                                                <td>2018-09-29 05:57 PM</td>
+                                                <!-- <td>05:57</td> -->
+                                                <td class="text-right">morocco</td>
+                                                <td class="text-start">usa</td>
+                                                <td class="text-right">tamama</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>test</td>
+                                                <td>2018-09-29 05:57 PM</td>
+                                                <!-- <td>05:57</td> -->
+                                                <td class="text-right">morocco</td>
+                                                <td class="text-start">usa</td>
+                                                <td class="text-right">tamama</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>test</td>
+                                                <td>2018-09-29 05:57 PM</td>
+                                                <!-- <td>05:57</td> -->
+                                                <td class="text-right">morocco</td>
+                                                <td class="text-start">usa</td>
+                                                <td class="text-right">tamama</td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+          
 <div class="container-fluid">
   <div class="row">
     <div class="col-lg-12">
