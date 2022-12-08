6169be4acb32613aa7d6c7ec481bc70c98f3a285
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2022-12-08 17:43:13 +0100

    merge

--- pages/stadpage.php
+++ pages/stadpage.php
@@ -1,207 +1,84 @@
-<?php 
-include '../include/navbar.php';
+<?php include_once '../scripts.php/landingpage.script.php';?>
+    
+
+<?php include_once '../include/head.php'; ?>
+<?php include_once '../include/navbar.php'; ?>
+<link rel="stylesheet" href="../assets/css/landing_page.css">
 
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../assets/css/stadpage.css">
-    <title>Document</title>
-</head>
-<body>
     <div class=" container my-3">
-        <div class="row my-2  ">
-            lkljlkj
-            <!-- cet card pour les équipes  -->
-                <div class="col-lg-3 col-md-6 col-sm-12 my-1 ">
-                    <div class="card ">
-                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
-                        <div class="card-body">
-                            <div>Morocco National Teames</div>
-                            <div>Group F</div>
-                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
+        <!-- cet card pour les match  -->
+        <div class=" row my-2 my-ms-1">
+            <?php if(isset($_POST['display-Machts'])):?>
+                <?php foreach($matchs as $match): ?>
+                    <div class="col-lg-3 col-md-6 col-sm-12 my-1">
+                        <div class="card ">
+                            <div class="position-relative">
+                                <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
+                                <img class=" position-absolute top-50 start-0 translate-middle-y" style="width: 6rem;"  src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
+                                <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
+                            </div>
+                            <div class="card-body">
+                                <div class="d-flex">
+                                    <div class=" d-flex align-items-center">
+                                        NOV<br>25
+                                    </div>
+                                    <div class="ms-5">
+                                        <div>morocco vs canada</div>
+                                        <div>$ 150</div>
+                                        <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
-                        <div class="card-body">
-                            <div>Morocco National Teames</div>
-                            <div>Group F</div>
-                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
-                        <div class="card-body">
-                            <div>Morocco National Teames</div>
-                            <div>Group F</div>
-                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <img class=" card-img-top" alt="100%x280" src="https://i.ytimg.com/vi/PgtNCDpDJeA/sddefault.jpg">
-                        <div class="card-body">
-                            <div>Morocco National Teames</div>
-                            <div>Group F</div>
-                            <i class="fa-solid fa-location-dot"></i> <span>Morocco</span>
-                        </div>
-                    </div>
-                </div>
+                <?php endforeach; ?>
+            <?php endif ?>
         </div>
-           <!-- cet card pour les match  -->
-           <div class=" row my-2 my-ms-1">
-                <div class="col-lg-3 col-md-6 col-sm-12 my-1">
-                    <div class="card ">
-                        <div class="position-relative">
-                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
-                            <img class=" position-absolute top-50 start-0 translate-middle-y" style="width: 6rem;"  src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
-                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
-                        </div>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class=" d-flex align-items-center">
-                                    NOV<br>25
-                                </div>
-                                <div class="ms-5">
-                                    <div>morocco vs canada</div>
-                                    <div>$ 150</div>
-                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
-                                </div>
+        
+
+        <!-- cet card pour les teames  -->
+        <div class="row my-2  ">
+            <?php if(isset($_POST['display-Teams'])):?>
+                <?php foreach($teams as $team): ?>
+                    <div class="col-lg-3 col-md-6 col-sm-12 my-1 ">
+                        <div class="card ">
+                            <img class=" card-img-top" alt="100%x280" src="../assets/img/teams/<?php echo $team->getImage()?>">
+                            <div class="card-body">
+                                <div><?php echo $team->getCountry() ?> National Teames</div>
+                                <div>Group F</div>
+                                <i class="fa-solid fa-location-dot"></i> <span><?php echo $team->getCountry() ?></span>
                             </div>
                         </div>
                     </div>
-                </div>
-            
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <div class="position-relative">
-                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
-                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
-                            <img class="position-absolute top-50 end-0 translate-middle-y"  style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
-                        </div>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class=" d-flex align-items-center">
-                                    NOV<br>25
-                                </div>
-                                <div class="ms-5">
-                                    <div>morocco vs canada</div>
-                                    <div>$ 150</div>
-                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
-                                </div>
+                <?php endforeach; ?>
+            <?php endif ?>
+        </div>
+
+
+        
+        <!-- cet card pour les stades  -->
+        <div class="row my-2  ">
+            <?php if(isset($_POST['display-Stads'])):?>
+                <?php foreach($stads as $stad): ?>
+                    <div class="col-lg-3 col-md-6 col-sm-12 my-1">
+                        <div class="card">
+                            <img class="card-img-top" alt="100%x280" src="../assets/img/stad/<?php echo($stad->getImage()) ;?>">
+                            <div class="card-body">
+                                <div><?php echo($stad->getName()) ;?></div>
+                                <div class="mb-1">capacity : <span><?php echo($stad->getCapacity()) ;?></span></div>
+                                <i class="fa-solid fa-location-dot"></i><spain> <?php echo($stad->getCity()) ;?></spain>
                             </div>
                         </div>
                     </div>
-                </div>
-            
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <div class="position-relative">
-                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
-                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
-                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
-                        </div>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class=" d-flex align-items-center">
-                                    NOV<br>25
-                                </div>
-                                <div class="ms-5">
-                                    <div>morocco vs canada</div>
-                                    <div>$ 150</div>
-                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php endforeach; ?>
+            <?php endif ?>
+        </div>
+        
 
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card ">
-                        <div class="position-relative">
-                            <img class="card-img-top " alt="100%x280" src="https://img.freepik.com/premium-vector/football-2022-qatar-purple-background-isometric-vector-illustration_64478-1268.jpg">
-                            <img class=" position-absolute top-50 start-0 translate-middle-y"  style="width: 6rem;" src="https://www.nicepng.com/png/full/291-2914458_morocco-soccer-logo-morocco-national-football-team-logo.png" alt="">
-                            <img class="position-absolute top-50 end-0 translate-middle-y" style="width: 6rem;" src="https://upload.wikimedia.org/wikipedia/ar/thumb/3/31/Spain_National_Football_Team_badge.png/800px-Spain_National_Football_Team_badge.png" alt="">
-                        </div>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class=" d-flex align-items-center">
-                                    NOV<br>25
-                                </div>
-                                <div class="ms-5">
-                                    <div>morocco vs canada</div>
-                                    <div>$ 150</div>
-                                    <i class="fa-solid fa-location-dot"></i><spain> Ahmed ben Ali staduim</spain>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <!-- cet card pour les stades  -->
-            <div class=" row my-2 ">
-                <div class="col-lg-3 col-md-6 col-sm-12 my-1">
-                    <div class="card">
-                        <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
-                        <div class="card-body">
-                                <div>Khalifa International Stadium</div>
-                                <div>capacity : <span>40,000</span></div>
-                                <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card">
-                        <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
-                        <div class="card-body">
-                                <div>Khalifa International Stadium</div>
-                                <div>capacity : <span>40,000</span></div>
-                                <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                <div class="card">
-                    <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
-                    <div class="card-body">
-                            <div>Khalifa International Stadium</div>
-                            <div>capacity : <span>40,000</span></div>
-                            <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
-                    </div>
-                </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                <div class="card">
-                    <img class="card-img-top" alt="100%x280" src="https://www.arabianbusiness.com/cloud/2022/05/09/FIFA-World-Cup-2022-1.jpg">
-                    <div class="card-body">
-                            <div>Khalifa International Stadium</div>
-                            <div>capacity : <span>40,000</span></div>
-                            <i class="fa-solid fa-location-dot"></i><spain> Doha </spain>
-                    </div>
-                </div>
-                </div>
-            </div>
-           
     </div>
-    <?php      include '../include/footer.php';?>
+    <?php include_once '../include/footer.php';?>
 </body>
 </html>
