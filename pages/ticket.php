7390a1c73417ed5a5d3f6246cf95eb6ea09ae579
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-12-12 14:22:08 +0100

    ticket page | working

---
+++ pages/ticket.php
@@ -0,0 +1,28 @@
+  <link rel="stylesheet" href="../assets/css/styleF.css">
+  <?php include"../include/head.php" ?>
+
+<body class="container">
+    <div class="row">
+        <div class="custom-shape-divider-top-1670835323 col-sm-md-lg">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </div>
+    <div class="row justify-content-between mt-5">
+        <div class="col-6 mt-5">
+            <p>
+                <h1>first name</h1>
+                <h1>last  name</h1>
+                <h1>number of ticket</h1>
+                <h1>Price</h1>
+            </p>
+        </div>  
+        <div class="col-6 img">
+            <img src="../assets/img/general/ticket.png" alt="pecture of world cup 2022">
+        </div> 
+    </div>
+    <hr>
+    <a href="">print </a>
+</body>
+</html>
