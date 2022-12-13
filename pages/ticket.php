3effcef2542a0f91c3f10b09fc04b5c5ab6bdeb2
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-12-13 11:15:58 +0100

    responsive | print | tickets

--- pages/ticket.php
+++ pages/ticket.php
@@ -1,8 +1,12 @@
-  
-  <?php include"../include/head.php" ?>
+   
+  <?php 
+    
+    include_once '../include/head.php';
+
+  ?>
 
 <body class="container">
-    <div class="row">
+    <div class="row ">
         <div class="custom-shape-divider-top-1670835323 col-sm-md-lg">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
@@ -11,14 +15,14 @@
             </svg>
         </div>
     </div>
-    <div class="row justify-content-between mt-5   ">
+    <div class="row justify-content-between mt-5">
         <div class="mt-4 col-6   ">
-                    <div class="my-3  d-flex flex-column align-items-between  ">
-                        <h3 class=""> Morocco VS France</h3> <hr>
-                        <div><i class=" my-1 py-2 mr-2 fa-solid fa-location-dot"></i><span class="ms-3">Location</span></div>
-                        <div><i class="  text-start mr-2 fa-solid fa-calendar-days"></i><span class="ms-3">xx/xx/xxxx xx:xx</span></div>
-                        <p class="my-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus debitis repudiandae non! Laborum</p>
-                    </div>
+            <div class="my-3  d-flex flex-column align-items-between  ">
+                <h3 class=""> Morocco VS France</h3> <hr>
+                <div><i class=" my-1 py-2 mr-2 fa-solid fa-location-dot"></i><span class="ms-3">Location</span></div>
+                <div><i class="  text-start mr-2 fa-solid fa-calendar-days"></i><span class="ms-3">xx/xx/xxxx xx:xx</span></div>
+                <p class="my-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus debitis repudiandae non! Laborum</p>
+            </div>
         </div>
         <div class="col-6 img">
             <img src="../assets/img/general/ticket.png" alt="pecture of world cup 2022">
@@ -26,7 +30,7 @@
     </div>
     <hr>
     <div class="row justify-content-center">
-        <div class="col-12">
+        <div class="col-12 print-btn">
             <button
                         type="submit"
                         id="cancel-save"
@@ -37,7 +41,7 @@
             </button>
             <button
                         type="submit"
-                        id="#print-btn"
+                        id="print-btn"
                         onclick="window.print();"
                         class="btn btn-primary"
                     >
@@ -56,10 +60,7 @@
 
 
 
-<?php 
-include_once '../scripts.php/landingpage.script.php';
-include_once '../include/head.php';
-?>
+
 <!-- 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
