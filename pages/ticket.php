0d3de3ab426540ecd483915d9f221f5ec3c11a07
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-12-13 11:36:18 +0100

    refactoring | ticket

--- pages/ticket.php
+++ pages/ticket.php
@@ -1,16 +1,13 @@
    
-  <?php 
-    
+<?php 
     include_once '../include/head.php';
-
-  ?>
-
+?>
 <body class="container">
     <div class="row ">
         <div class="custom-shape-divider-top-1670835323 col-sm-md-lg">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
-                      class="shape-fill">
+                      class="shape-fill ms-3">
                     </path>
             </svg>
         </div>
@@ -30,20 +27,12 @@
     </div>
     <hr>
     <div class="row justify-content-center">
-        <div class="col-12 print-btn">
-            <button
-                        type="submit"
-                        id="cancel-save"
-                        name="cancel_save"
-                        class="btn btn-secondary"
-                    >
-                        Cancel
-            </button>
+        <div class="col-12 print-btn text-center">
             <button
                         type="submit"
                         id="print-btn"
                         onclick="window.print();"
-                        class="btn btn-primary"
+                        class="btn btn-warning"
                     >
                     print your ticket
             </button>
@@ -52,57 +41,3 @@
 
 </body>
 </html> 
-
-
-
-
-
-
-
-
-
-<!-- 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="../assets/css/ticketspage.css"><link rel="stylesheet" type="text/css" href="../assets/css/styleF.css" media="print">
-
-    <div class="container">
-   
-
-        <div class=" container  d-flex justify-content-between  flex-wrap  w-100 my-5  ">
-        <div class="custom-shape-divider-top-1670835323 col-sm-md-lg">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
-                      class="shape-fill">
-                    </path>
-            </svg>
-        </div>
-            <div class=" col-1 d-flex  flex-md-column align-items-sm-center ">
-                <spain class=" d-none d-md-block  my-2   text-center  " >Share </spain>
-                <i class="icon my-2 m-2 fa-solid fa-link"></i>
-                <i class="icon my-2 m-2 fa-brands fa-square-instagram"></i>
-                <i class="icon my-2 m-2 fa-brands fa-facebook"></i>
-                <i class=" icon  my-2 m-2  fa-brands fa-square-twitter"></i>
-            </div>
-            <div class="col-12 col-md-11 d-flex flex-column " >
-                <div  class="image-conatiner p-1 d-flex justify-content-center">
-         
-                    
-                </div>
-                <div class=" mt-4  d-flex justify-content-between flex-wrap  ">
-                    <div class="my-3  d-flex flex-column align-items-between  ">
-                        <h3 class=""> morocco vs france</h3>
-                        <div><i class=" my-1 py-2 mr-2 fa-solid fa-location-dot"></i><span>location</span></div>
-                        <div><i class="  text-start mr-2 fa-solid fa-calendar-days"></i><span>xx/xx/xxxx xx:xx</span></div>
-                        <p class="my-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus debitis repudiandae non! Laborum</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    
-
-</body>
-</html> -->
